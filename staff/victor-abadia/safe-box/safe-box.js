b2793ee57aa3909e2aecf1cbb55a36b50ba4e14b
Víctor <abadia.vic@gmail.com>
2018-10-04 15:58:29 +0200

    add some code in safe-box.js

---
+++ staff/victor-abadia/safe-box/safe-box.js
@@ -0,0 +1,11 @@
+// safe-box.js
+
+var safeBox = {
+    saveSecret: function(secret, password) {
+        var secret = "this is my secret";   
+    },
+
+    retrieveSecret: function(password) {
+        // TODO
+    }
+}
