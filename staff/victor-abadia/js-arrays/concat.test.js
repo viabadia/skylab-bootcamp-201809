298804eaa3079000242cf3d58d5e62bc91b5d28a
Víctor <abadia.vic@gmail.com>
2018-10-04 14:09:15 +0200

    add changes concat test

---
+++ staff/victor-abadia/js-arrays/concat.test.js
@@ -0,0 +1,36 @@
+console.log('TEST concat');
+
+var tests = [];
+
+tests.push(function () {
+    var arr = [1, 2, 3];
+
+    console.log('first argument should be an array');
+
+    if (!(arr instanceof Array)) throw Error('array is not valid');
+});
+
+tests.push(function () {
+    var arr2 = [1, 2, 3];
+
+    console.log('second argument should be an array');
+
+    if (!(arr2 instanceof Array)) throw Error('arr2 is not an array');
+});
+
+tests.push(function () {
+    var arr = [1, 2, 3];
+
+    console.log('first array should be contain elements');
+
+    if (arr.length === 0) throw Error('the first array contains nothing');
+});
+tests.push(function () {
+    var arr2 = [1, 2, 3];
+
+    console.log('second array should be contain elements');
+
+    if (arr2.length === 0) throw Error('the second array contains nothing');
+});
+
+testSuite(tests);
